937d06d8d068b03ad63d6832bd6e9c147251e05d
Barot Pratham Janakkumar <barotpratham30@gmail.com>
2022-09-28 17:53:54 +0530

    added new module view order, ticket page validation

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  
   return (
+   
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
